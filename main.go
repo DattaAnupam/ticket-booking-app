8d49673133b6729f008a311a95ab23498a8021e8
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-08-19 13:08:34 +0530

    format print statement

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	const noOfTickets int = 50
 	var remaingTickets int = 50
 
-	fmt.Println("Welcome to our", conferenceName, "booking application")
-	fmt.Println("We have total of", noOfTickets, "tickets and", remaingTickets, "tickets are available")
+	fmt.Printf("Welcome to our %s booking application\n", conferenceName)
+	fmt.Printf("We have total of %d tickets and %d tickets are available\n", noOfTickets, remaingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
